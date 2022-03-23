cb03a5bd762f15749395ce07b6604c1f6551be4b
Zane <zane.sausverde@gmail.com>
2022-03-23 12:09:10 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// programm - prints out 25% of what every past into it
+         double Precentage = 0.25;
+         int AnyValue = 300; // variable value
+
+        System.out.println("Precentage * AnyWalue=" + (Precentage*AnyValue));
+
+    }
+}
